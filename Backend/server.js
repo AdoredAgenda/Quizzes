ef8fe523ea968d0c70dcf527002c9e788a9c99a3
himanshu gupta <himanshugupta@himanshus-MacBook-Air.local>
2023-09-01 23:55:12 +0530

    errors resolved

--- Backend/server.js
+++ Backend/server.js
@@ -38,11 +38,12 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
+
 const server = http.createServer(app);
 server.listen(port, ip, () => {
   console.log(`Server running at http://${ip}:${port}/`);
 });
-const io = require("socket.io")(server, { cors: "*" });
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 io.on("connect", (socket) => {
   socket.on("registerUser", (data, cb) => {
     registerUser(socket, data, cb);
