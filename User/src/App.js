28cecef114174c8cd6e3e1acf50ea7a36328b8a4
Halleys123 <65032719+Halleys123@users.noreply.github.com>
2023-09-01 14:56:36 +0530

    User side almost complete

--- User/src/App.js
+++ User/src/App.js
@@ -1,12 +1,80 @@
 import styles from "./App.module.css";
-import { useEffect } from "react";
-import img from "./Assets/Illustration.png";
+import { useEffect, useState, useReducer } from "react";
+import Welcome from "./components/Welcome/Welcome";
+import Login from "./components/Login/Login";
+import Waiting from "./components/Waiting/Waiting";
+import QuestionPage from "./components/QuestionPage/QuestionPage";
+
 function App() {
   let userData = {
     name: "",
     rollNo: "",
     score: 0,
   };
+  //change page reducer
+  const changePage = (state, action) => {
+    console.log(state, action);
+    switch (action.type) {
+      case "next":
+        return state + 1;
+      case "prev":
+        return state - 1;
+      default:
+        return state;
+    }
+  };
+  const [page, dispatch] = useReducer(changePage, 4);
+  let pages = [
+    {
+      name: "Welcome",
+      page: 1,
+      jsx: (
+        <Welcome
+          changePage={(type) => {
+            dispatch({ type: type });
+          }}
+          key="welcome"
+        />
+      ),
+    },
+    {
+      name: "Login",
+      page: 2,
+      jsx: (
+        <Login
+          key="login"
+          changePage={(type) => {
+            dispatch({ type: type });
+          }}
+        />
+      ),
+    },
+    {
+      name: "Waiting",
+      page: 3,
+      jsx: (
+        <Waiting
+          key="waiting"
+          changePage={(type) => {
+            dispatch({ type: type });
+          }}
+        />
+      ),
+    },
+    {
+      name: "QuestionPage",
+      page: 4,
+      jsx: (
+        <QuestionPage
+          key="questionPage"
+          changePage={(type) => {
+            dispatch({ type: type });
+          }}
+        />
+      ),
+    },
+  ];
+
   // useEffect(() => {
   //   let token;
   //   if (localStorage.get("token") === null) {
@@ -22,15 +90,11 @@ function App() {
 
   return (
     <div className={styles.App}>
-      <div className={styles.header}>
-        <img src={img} alt="Google Logo" />
-      </div>
-      <div className={styles.main}>
-        <span className={styles.heading}>Interesting QUIZ Awaits You</span>
-        <span className={styles.text}>
-          play quizzes with your friends and get various prizes
-        </span>
-      </div>
+      {pages.map((item) => {
+        if (page === item.page) {
+          return item.jsx;
+        }
+      })}
     </div>
   );
 }
---
+++ User/src/components/Waiting/Waiting.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Waiting.module.css";
+import img from "../../Assets/bg2.png";
+import clock from "../../Assets/Clock.svg";
+
+export default function Waiting({ changePage }) {
+  return (
+    <React.Fragment>
+      <div className={styles.img}>
+        <img src={img} alt="Google Logo" />
+      </div>
+      <div className={styles.background}>
+        <div className={styles.icon}>
+          <img src={clock} alt="Clock" />
+        </div>
+        <span className={styles.heading}>
+          Waiting for the event to start...
+        </span>
+      </div>
+    </React.Fragment>
+  );
+}
