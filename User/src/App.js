95c484e4ee663a915cb69e64b3006e1fb3f82545
Halleys123 <65032719+Halleys123@users.noreply.github.com>
2023-09-02 14:02:18 +0530

    Some more bug fixes

--- User/src/App.js
+++ User/src/App.js
@@ -14,6 +14,8 @@ export default function App() {
       case "next":
         return state + 1;
       case "prev":
+        setMessage(action.message);
+        setScore(action.score);
         return state - 1;
       case "two":
         return state + 2;
@@ -21,7 +23,8 @@ export default function App() {
         return state;
     }
   }
-
+  const [score, setScore] = useState(0);
+  const [message, setMessage] = useState("");
   const [socket, setSocket] = useState(null);
   const [page, dispatch] = useReducer(changePage, 1);
   const [loggedIn, setLoggedIn] = useState(false);
@@ -108,6 +111,8 @@ export default function App() {
       jsx: (
         <Waiting
           key="waiting"
+          message={message}
+          score={score}
           changePage={(type) => {
             dispatch({ type: type });
           }}
--- User/src/components/Waiting/Waiting.js
+++ User/src/components/Waiting/Waiting.js
@@ -3,7 +3,7 @@ import styles from "./Waiting.module.css";
 import img from "../../Assets/bg2.png";
 import clock from "../../Assets/Clock.svg";
 
-export default function Waiting({ changePage }) {
+export default function Waiting(props) {
   return (
     <React.Fragment>
       <div className={styles.img}>
@@ -13,7 +13,11 @@ export default function Waiting({ changePage }) {
         <div className={styles.icon}>
           <img src={clock} alt="Clock" />
         </div>
-        <span className={styles.heading}>Please Wait...</span>
+
+        <span className={styles.heading}>{props.score}</span>
+        <span className={`${styles.score} ${styles.top}`}>
+          {props.message || "Please Wait..."}
+        </span>
       </div>
     </React.Fragment>
   );
